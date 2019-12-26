a32fe86160964dd55bb176338ea8644c1d0c949d
PARK <realeap91@gmail.com>
2019-12-26 16:46:57 +0900

    set basic Routers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,21 @@
 import express from "express";
+import routes from "../routes";
+import {
+    users,
+    userDetail,
+    editProfile,
+    changePassword
+  } from "../controllers/userController";
 const userRouter = express.Router();
 
-export default userRouter;
-
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 // model is data
+export default userRouter;
 // view how does the data look
 // c function that looks for the data
+/* EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+*/
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,3 +1,25 @@
 import express from "express";
+import routes from "../routes";
+import {
+    videos,
+    upload,
+    videoDetail,
+    editVideo,
+    deleteVideo
+  } from "../controllers/videoController";
 var videoRouter = express.Router();
 export default videoRouter;
+
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+/* 
+const VIDEOS = "/videos";
+const UPLOAD = "/upload";
+const VIDEO_DETAIL = "/:id";
+const EDIT_VIDEO = "/:id/edit";
+const DELETE_VIDEO = "/:id/delete";
+*/
